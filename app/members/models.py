9af90605f02aff47b0713e7ef84112d4f5db994e
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-30 22:11:09 +0900

    User model create, admin settings write

--- app/members/models.py
+++ app/members/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    profile_image = models.ImageField(blank=True)
+    site = models.URLField(blank=True)
+    email = models.EmailField()
+    CHOICE_GENDER = (
+        ('m', '남성'),
+        ('f', '여성'),
+        ('x', '선택하지 않습니다.')
+    )
+    gender = models.CharField(max_length=1, choices=CHOICE_GENDER)
+    created_at = models.DateTimeField(auto_now_add=True)
+
