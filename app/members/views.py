ee863200f8aa4b73de536751ceec3c7b8548ec6b
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-01 18:31:14 +0900

    signup, signin, signout 구현

--- app/members/views.py
+++ app/members/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from .forms import SignupForm
 
-# Create your views here.
+def sign_in(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+    return render(request, 'sign/sign_in.html')
+
+def sign_up(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.created_user()
+            login(request, user)
+            return redirect('index')
+    context = {
+        'form':form,
+    }
+    return render(request, 'sign/sign_up.html', context)
+
+def sign_out(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('index')
+
