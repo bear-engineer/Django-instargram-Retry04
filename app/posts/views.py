8fb5098e9ef8f1f57ebf2c78e94980bccced074c
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-01 19:32:33 +0900

    post delete 구현

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 
 from posts.forms import PostForm, PostCreateForm
@@ -10,6 +12,7 @@ def post_list(request):
     }
     return render(request, 'posts/post_list.html', context)
 
+@login_required(redirect_field_name='posts:post-create')
 def post_create(request):
     form = PostCreateForm()
     if request.method == 'POST':
@@ -27,7 +30,14 @@ def post_create(request):
     return render(request, 'posts/post_create.html', context)
 
 def post_delete(request, pk):
-    pass
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        if request.user.username:
+            if post.author.username == request.user.username:
+                post.delete()
+            else:
+                raise PermissionDenied('지울수 있는 권한이 없습니다.')
+        else:redirect('members:sign-in')
 
 def comment_list(request):
     pass
